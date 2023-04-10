c7e22565d777aef00b437cbe422210b1886f4495
Mac <mac@MacBook-Air-Mac.local>
2023-04-10 15:51:17 +0900

    first commit

---
+++ Functions-1/2/index.js
@@ -0,0 +1,9 @@
+const bigImg = document.getElementById("big-img");
+
+const leftPhoto = () => {
+    bigImg.src = "https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg";
+};
+
+const rightPhoto = () => {
+    bigImg.src = "https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg"
+};
